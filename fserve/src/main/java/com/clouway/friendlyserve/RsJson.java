ffadca1e569fbac077b70262551884be358130b1
Miroslav Genov <mgenov@gmail.com>
2016-09-03 06:56:06 +0300

    encoding/json: json messages are now encoded in utf8
    JsonWriter is now using StandardEncodings.UTF-8 property.

--- fserve/src/main/java/com/clouway/friendlyserve/RsJson.java
+++ fserve/src/main/java/com/clouway/friendlyserve/RsJson.java
@@ -9,6 +9,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author Miroslav Genov (miroslav.genov@clouway.com)
@@ -30,7 +31,7 @@ public class RsJson extends RsWrap {
 
   private static InputStream streaming(JsonElement src) {
     final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    try (JsonWriter writer = new JsonWriter(new OutputStreamWriter(baos))) {
+    try (JsonWriter writer = new JsonWriter(new OutputStreamWriter(baos, StandardCharsets.UTF_8))) {
       GSON.toJson(src, writer);
     } catch (IOException e) {
       e.printStackTrace();
--- tests/src/test/java/com/clouway/friendlyserve/BuildJsonResponsesTest.java
+++ tests/src/test/java/com/clouway/friendlyserve/BuildJsonResponsesTest.java
@@ -1,14 +1,20 @@
 package com.clouway.friendlyserve;
 
 import com.clouway.friendlyserve.testing.RsPrint;
+import com.google.common.io.ByteStreams;
 import com.google.gson.JsonObject;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.StandardCharsets;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.fail;
 
 
 /**
@@ -31,6 +37,20 @@ public class BuildJsonResponsesTest {
     jsonObject.addProperty("::key2::", "::value2::");
     RsJson json = new RsJson(jsonObject);
     assertThat(new RsPrint(json).print(), is(equalTo("Content-Type: application/json\r\n{\"::key1::\":\"::value1::\",\"::key2::\":\"::value2::\"}")));
+  }
+
+  @Test
+  public void handleContentInUtf8() throws IOException {
+    JsonObject jsonObject = new JsonObject();
+    jsonObject.addProperty("::key1::", "тестова стойност");
+    RsJson json = new RsJson(jsonObject);
+
+    CharsetDecoder cs = StandardCharsets.UTF_8.newDecoder();
+    try {
+      cs.decode(ByteBuffer.wrap(ByteStreams.toByteArray(json.body())));
+    } catch (CharacterCodingException e) {
+      fail("decoding to utf8 failed with: " + e.getMessage());
+    }
   }
 
 }
