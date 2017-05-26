afe3250e7a62c589ef8986914253358b726d6a34
Miroslav Genov <mgenov@gmail.com>
2017-05-26 11:53:11 +0300

    testing: add builder for testing requests
    Added builder to easy testing of Requests.

--- testing/src/main/java/com/clouway/friendlyserve/testing/FakeRequest.java
+++ testing/src/main/java/com/clouway/friendlyserve/testing/FakeRequest.java
@@ -1,27 +1,88 @@
 package com.clouway.friendlyserve.testing;
 
+import com.clouway.friendlyserve.Cookie;
 import com.clouway.friendlyserve.Request;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class FakeRequest implements Request {
 
+  public static Builder aNewRequest() {
+    return new Builder();
+  }
+
+  public static class Builder {
+    private String path = "/";
+    private Map<String, String> params = new LinkedHashMap<>();
+    private Map<String, String> headers = new LinkedHashMap<>();
+    private List<Cookie> cookies = new LinkedList<>();
+    private byte[] body = new byte[] {  };
+
+    public Builder path(String path) {
+      this.path = path;
+      return this;
+    }
+
+    public Builder param(String key, String value) {
+      params.put(key, value);
+      return this;
+    }
+
+    public Builder params(Map<String, String> params) {
+      this.params = params;
+      return this;
+    }
+
+    public Builder header(String name, String value) {
+      headers.put(name, value);
+      return this;
+    }
+
+    public Builder headers(Map<String, String> headers) {
+      this.headers = headers;
+      return this;
+    }
+
+    public Builder cookie(Cookie cookie) {
+      cookies.add(cookie);
+      return this;
+    }
+
+    public Builder body(byte[] body) {
+      this.body = body;
+      return this;
+    }
+
+    public Request build() {
+      return new FakeRequest(path, params, headers, cookies, body);
+    }
+  }
+
+  private final String path;
   private final Map<String, String> params;
   private final Map<String, String> headers;
+  private final List<Cookie> cookies;
   private final byte[] body;
-  private final String path;
 
-  public FakeRequest(String path, Map<String, String> params, Map<String, String> headers, byte[] content) {
+
+  public FakeRequest(String path, Map<String, String> params, Map<String, String> headers, List<Cookie> cookies, byte[] content) {
     this.path = path;
     this.params = ImmutableMap.copyOf(params);
-    this.body = Arrays.copyOf(content, content.length);
     this.headers = ImmutableMap.copyOf(headers);
+    this.cookies = cookies;
+    this.body = Arrays.copyOf(content, content.length);
+  }
+
+  public FakeRequest(String path, Map<String, String> params, Map<String, String> headers, byte[] content) {
+    this(path, params, headers, Collections.<Cookie>emptyList(), content);
   }
 
   public FakeRequest(Map<String, String> params, Map<String, String> headers) {
@@ -50,7 +111,13 @@ public class FakeRequest implements Request {
 
   @Override
   public Iterable<String> cookie(String name) {
-    return Lists.newLinkedList();
+    List<String> values = new LinkedList<>();
+    for (Cookie each : cookies) {
+      if (each.name().equalsIgnoreCase(name)) {
+        values.add(each.value());
+      }
+    }
+    return values;
   }
 
   @Override
