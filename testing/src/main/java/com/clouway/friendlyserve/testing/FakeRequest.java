633228df9ac33165b5e47c577447a6f411f5043e
Miroslav Genov <mgenov@gmail.com>
2017-05-27 16:37:45 +0300

    fserve: FakeRequest now implements RequestExt
    The testing request now could be casted to ReqestExt. This is required
    for rare cases when the exteded request definition is required in the
    production code.

--- testing/src/main/java/com/clouway/friendlyserve/testing/FakeRequest.java
+++ testing/src/main/java/com/clouway/friendlyserve/testing/FakeRequest.java
@@ -2,32 +2,75 @@ package com.clouway.friendlyserve.testing;
 
 import com.clouway.friendlyserve.Cookie;
 import com.clouway.friendlyserve.Request;
-import com.google.common.collect.ImmutableMap;
+import com.clouway.friendlyserve.RequestExt;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-public class FakeRequest implements Request {
+public class FakeRequest implements RequestExt {
 
   public static Builder aNewRequest() {
     return new Builder();
   }
 
+
   public static class Builder {
+    private String method = "GET";
     private String path = "/";
+    private String url = "http://localhost:8080";
+    private String queryString = null;
+    private String contentType = "text/html";
+    private String remoteAddress = "10.10.10.10";
     private Map<String, String> params = new LinkedHashMap<>();
     private Map<String, String> headers = new LinkedHashMap<>();
     private List<Cookie> cookies = new LinkedList<>();
-    private byte[] body = new byte[] {  };
+    private byte[] body = new byte[]{};
+
+    public Builder get(String path) {
+      this.method = "GET";
+      this.path = path;
+      return this;
+    }
+
+    public Builder post(String path) {
+      this.method = "POST";
+      this.path = path;
+      return this;
+    }
+
+    public Builder put(String path) {
+      this.method = "PUT";
+      this.path = path;
+      return this;
+    }
+
+    public Builder delete(String path) {
+      this.method = "DELETE";
+      this.path = path;
+      return this;
+    }
+
+    public Builder query(String queryString) {
+      this.queryString = queryString;
+      return this;
+    }
 
     public Builder path(String path) {
       this.path = path;
+      return this;
+    }
+
+    public Builder contentType(String contentType) {
+      this.contentType = contentType;
+      return this;
+    }
+
+    public Builder remoteAddress(String remoteAddress) {
+      this.remoteAddress = remoteAddress;
       return this;
     }
 
@@ -35,6 +78,7 @@ public class FakeRequest implements Request {
       params.put(key, value);
       return this;
     }
+
 
     public Builder params(Map<String, String> params) {
       this.params = params;
@@ -56,17 +100,27 @@ public class FakeRequest implements Request {
       return this;
     }
 
+    public Builder cookies(List<Cookie> cookies) {
+      this.cookies = cookies;
+      return this;
+    }
+
     public Builder body(byte[] body) {
       this.body = body;
       return this;
     }
 
     public Request build() {
-      return new FakeRequest(path, params, headers, cookies, body);
+      return new FakeRequest(this);
     }
   }
 
+  private final String method;
+  private final String url;
   private final String path;
+  private final String queryString;
+  private final String contentType;
+  private final String remoteAddress;
   private final Map<String, String> params;
   private final Map<String, String> headers;
   private final List<Cookie> cookies;
@@ -74,23 +128,32 @@ public class FakeRequest implements Request {
 
 
   public FakeRequest(String path, Map<String, String> params, Map<String, String> headers, List<Cookie> cookies, byte[] content) {
-    this.path = path;
-    this.params = ImmutableMap.copyOf(params);
-    this.headers = ImmutableMap.copyOf(headers);
-    this.cookies = cookies;
-    this.body = Arrays.copyOf(content, content.length);
+    this(aNewRequest().path(path).params(params).headers(headers).cookies(cookies).body(content));
   }
 
   public FakeRequest(String path, Map<String, String> params, Map<String, String> headers, byte[] content) {
-    this(path, params, headers, Collections.<Cookie>emptyList(), content);
+    this(aNewRequest().path(path).params(params).headers(headers).body(content));
   }
 
   public FakeRequest(Map<String, String> params, Map<String, String> headers) {
-    this("/", params, headers, new byte[]{});
+    this(aNewRequest().path("/").params(params).headers(headers).body(new byte[]{}));
   }
 
   public FakeRequest(String path, Map<String, String> params, byte[] content) {
-    this(path, params, new LinkedHashMap<String, String>(), content);
+    this(aNewRequest().path(path).params(params).body(content));
+  }
+
+  FakeRequest(Builder builder) {
+    this.method = builder.method;
+    this.url = builder.url;
+    this.path = builder.path;
+    this.params = builder.params;
+    this.headers = builder.headers;
+    this.cookies = builder.cookies;
+    this.body = builder.body;
+    this.queryString = builder.queryString;
+    this.contentType = builder.contentType;
+    this.remoteAddress = builder.remoteAddress;
   }
 
 
@@ -129,4 +192,44 @@ public class FakeRequest implements Request {
   public InputStream body() {
     return new ByteArrayInputStream(body);
   }
+
+  @Override
+  public String method() {
+    return method;
+  }
+
+  @Override
+  public StringBuffer url() {
+    return new StringBuffer(url);
+  }
+
+  @Override
+  public String queryString() {
+    return queryString;
+  }
+
+  @Override
+  public String contentType() {
+    return contentType;
+  }
+
+  @Override
+  public Iterable<String> headerNames() {
+    return headers.keySet();
+  }
+
+  @Override
+  public Map<String, String[]> params() {
+    Map<String, String[]> result = new LinkedHashMap<>();
+    for (String each : params.keySet()) {
+      result.put(each, new String[]{params.get(each)});
+    }
+    return result;
+  }
+
+  @Override
+  public String remoteAddress() {
+    return remoteAddress;
+  }
+
 }
