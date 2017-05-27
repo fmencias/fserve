12737d39a6c90802bd8ac143c242d959c15bfab0
Miroslav Genov <mgenov@gmail.com>
2017-05-27 19:38:42 +0300

    fserve: add support for temporary and permanent redirects

--- fserve/src/main/java/com/clouway/friendlyserve/RsRedirect.java
+++ fserve/src/main/java/com/clouway/friendlyserve/RsRedirect.java
@@ -7,16 +7,31 @@ import java.net.HttpURLConnection;
  */
 public class RsRedirect extends RsWrap {
 
+  public static RsRedirect permanently(String redirectUrl) {
+    return new RsRedirect(redirectUrl, HttpURLConnection.HTTP_MOVED_PERM);
+  }
+  
+  public static RsRedirect temporary(String redirectUrl) {
+    return new RsRedirect(redirectUrl, HttpURLConnection.HTTP_MOVED_TEMP);
+  }
+
+
   private final String redirectUrl;
+  private final int statusCode;
 
   public RsRedirect(String redirectUrl) {
+    this(redirectUrl, HttpURLConnection.HTTP_MOVED_TEMP);
+  }
+
+  private RsRedirect(String redirectUrl, int statusCode) {
     super(new RsEmpty());
     this.redirectUrl = redirectUrl;
+    this.statusCode = statusCode;
   }
 
   @Override
   public Status status() {
-    return new Status(HttpURLConnection.HTTP_MOVED_TEMP, redirectUrl);
+    return new Status(statusCode, redirectUrl);
   }
 
 }
